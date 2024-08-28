e251894310c87e94966c6bf057e6a6ae9ead4ae8
Jonathan2132 <jbcllc488@gmail.com>
2024-08-28 04:16:44 -0400

    f

--- raros/ILIKE.sql
+++ raros/ILIKE.sql
@@ -1,6 +1,6 @@
 --- En PostgreSQL, ILIKE es una extensión del operador LIKE que permite realizar búsquedas de texto sin distinguir entre mayúsculas y minúsculas.
 SELECT * FROM usuarios
-WHERE nombre ILIKE '%juan%'kkokokok
+WHERE nombre ILIKE '%juan%';
 
 
 
